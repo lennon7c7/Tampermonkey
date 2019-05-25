083a1d5519ecbe5144795ec241c7c447e1404db7
lennon <bawgkh@qq.com>
2019-05-25 11:43:47 +0800

    modify 去掉下载按钮，访问页面自动导出TXT

--- txt-downloader.user.js
+++ txt-downloader.user.js
@@ -2,93 +2,80 @@
 // @name         文本下载
 // @namespace    https://boxnovel.baidu.com/
 // @version      1.0
-// @description  生成下载按钮，导出成TXT文件
+// @description  访问页面，自动导出TXT文件
 // @author       Lennon
 // @match        https://boxnovel.baidu.com/boxnovel/*
-// @require      http://code.jquery.com/jquery-2.1.1.min.js
+// @require      https://code.jquery.com/jquery-2.1.1.min.js
+// @require      https://js.zapjs.com/js/download.js
 // @run-at       document-end
 // @icon         https://s.cn.bing.net/th?id=OJ.F0zB41fAcHvL1g&pid=MsnJVFeeds&w=16&h=16
 // ==/UserScript==
-(function () {
-    'use strict';
+'use strict';
 
-    var myAwesomeScript = document.createElement('script');
-    myAwesomeScript.setAttribute('src', 'https://code.jquery.com/jquery-2.1.1.min.js');
-    document.head.appendChild(myAwesomeScript);
+var novelUrl = '';
+var novelBookid = '';
+var novelChapterid = '';
+var novelFilename = '';
+var novelTitle = '';
+var novelContent = '';
 
-    var url = '';
-    var gid = '';
-    var cid = '';
-    var title = '';
-    var content = '';
-
-    setTimeout(function () {
-        if (location.host === 'boxnovel.baidu.com') {
-            url = 'https://boxnovel.baidu.com/boxnovel/wiseapi/chapterContent';
-            gid = getQueryVariable('gid');
-            cid = getQueryVariable('cid');
-            title = '';
-            content = '';
-            if (document.querySelectorAll('.header')[1]) {
-                title = document.querySelectorAll('.header')[1].textContent;
-            } else if (document.querySelector('title')) {
-                title = document.querySelector('title').textContent;
-            }
-
-            siteBaidu(cid);
-        }
-    }, 3000);
-
-    function siteBaidu(next_cid) {
-        if (!next_cid) {
-            return;
+setTimeout(function () {
+    if (location.host === 'boxnovel.baidu.com') {
+        novelUrl = 'https://boxnovel.baidu.com/boxnovel/wiseapi/chapterContent';
+        novelBookid = getQueryVariable('gid');
+        novelChapterid = getQueryVariable('cid');
+        if (document.querySelectorAll('.header')[1]) {
+            novelTitle = document.querySelectorAll('.header')[1].textContent;
+        } else if (document.querySelector('title')) {
+            novelTitle = document.querySelector('title').textContent;
         }
 
-        $.ajax({
-            url: url,
-            data: {
-                bookid: gid,
-                cid: next_cid,
-            },
-            async: false,
-            dataType: 'json',
-            type: 'GET',
-            success: function (res) {
-                content += `${res.data.title}\n`;
-                res.data.content.forEach(function (value) {
-                    content += `${value}\n`;
-                });
-                content += `\n\n`;
+        siteBaidu(novelChapterid);
+    }
+}, 3000);
 
-                if (!res.data.pt.next_cid) {
-                    addDownloadButton(`${title}.txt`, content);
-                    return;
-                }
-
-                setTimeout(function () {
-                    siteBaidu(res.data.pt.next_cid);
-                }, 100);
-            }
-        });
+function siteBaidu(next_cid) {
+    if (!next_cid) {
+        return;
     }
 
-    function addDownloadButton(filename, text) {
-        var element = document.createElement('a');
-        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-        element.setAttribute('download', filename);
-        element.innerText = '下载TXT';
-        document.body.prepend(element);
-    }
+    $.ajax({
+        url: novelUrl,
+        data: {
+            bookid: novelBookid,
+            cid: next_cid,
+        },
+        async: false,
+        dataType: 'json',
+        type: 'GET',
+        success: function (res) {
+            novelContent += `${res.data.title}\n`;
+            res.data.content.forEach(function (value) {
+                novelContent += `${value}\n`;
+            });
+            novelContent += `\n\n`;
 
-    function getQueryVariable(variable) {
-        var query = window.location.search.substring(1);
-        var vars = query.split('&');
-        for (var i = 0; i < vars.length; i++) {
-            var pair = vars[i].split('=');
-            if (pair[0] === variable) {
-                return pair[1];
+            if (!res.data.pt.next_cid) {
+                novelFilename = `${novelTitle}.txt`;
+                download(novelContent, novelFilename, 'text/plain');
+                return;
             }
+
+            setTimeout(function () {
+                siteBaidu(res.data.pt.next_cid);
+            }, 100);
         }
-        return (false);
+    });
+}
+
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (pair[0] === variable) {
+            return pair[1];
+        }
     }
-})();
+    return (false);
+}
