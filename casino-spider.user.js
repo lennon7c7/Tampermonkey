22cf51f970b698459976929b8123fb6ca0443afc
lennon <lennonandjune@gmail.com>
2024-03-29 17:32:15 +0800

    feat: async await

--- casino-spider.user.js
+++ casino-spider.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         盘口分析
+// @name         365分析
 // @version      1.0
 // @description
 // @author       Lennon
@@ -12,23 +12,28 @@
 // @connect      127.0.0.1:31050
 // @run-at       document-end
 // ==/UserScript==
-'use strict';
+(function () {
+    'use strict';
 
-console.debug('---------- start ----------');
+    console.debug('---------- start ----------');
 
-var remoteScript = document.createElement('script');
-remoteScript.src = 'https://code.jquery.com/jquery-2.1.1.min.js';
-document.body.appendChild(remoteScript);
+    var remoteScript = document.createElement('script');
+    remoteScript.src = 'https://code.jquery.com/jquery-2.1.1.min.js';
+    document.head.appendChild(remoteScript);
 
-function initBet365() {
-    let bet365 = new Bet365(GM_xmlhttpRequest);
-    bet365.start()
-}
 
-remoteScript = document.createElement('script');
-remoteScript.src = 'http://127.0.0.1:31050/static/js/bet365.js?v=' + (+new Date());
-remoteScript.onload = initBet365;
-document.body.appendChild(remoteScript);
+    function initBet365() {
+        let bet365 = new Bet365(GM_xmlhttpRequest);
+        bet365.start()
+    }
 
-console.debug('---------- end ----------');
+    setTimeout(function () {
+        remoteScript = document.createElement('script');
+        remoteScript.src = 'http://127.0.0.1:31050/static/js/bet365.js?v=' + (+new Date());
+        remoteScript.onload = initBet365;
+        document.head.appendChild(remoteScript);
+    }, 3000);
 
+
+    console.debug('---------- end ----------');
+})();
