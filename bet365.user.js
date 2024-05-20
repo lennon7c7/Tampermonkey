42c6c7afd4507b8fc2ec603069bb6b7b4db595b5
lennon <lennonandjune@gmail.com>
2024-05-20 14:47:59 +0800

    feat: bet365.user.js doUpdateGame

--- bet365.user.js
+++ bet365.user.js
@@ -35,6 +35,25 @@ function timestamp2Date(timestamp) {
     return oYear + '-' + setZeroFill(oMonth) + '-' + setZeroFill(oDay);
 }
 
+/**
+ * 时间戳转日期时间
+ * @param {int} timestamp
+ * @returns {string}
+ */
+function timestamp2Gametime(timestamp) {
+    if (!timestamp) {
+        return '';
+    }
+
+    let oDate = new Date(timestamp * 1000),
+        oYear = oDate.getFullYear(),
+        oMonth = oDate.getMonth() + 1,
+        oDay = oDate.getDate(),
+        oHour = oDate.getHours(),
+        oMin = oDate.getMinutes();
+    return oYear + '-' + setZeroFill(oMonth) + '-' + setZeroFill(oDay) + ' ' + setZeroFill(oHour) + ':' + setZeroFill(oMin);
+}
+
 async function curlGet(reqUrl) {
     return await fetch(reqUrl)
         .then((response) => response.json())
@@ -98,11 +117,30 @@ async function apiSqlExec(sql) {
 }
 
 async function doUpdate() {
-    await doUpdateScheduleGroup();
-    await doUpdateTeam();
+    // 创建起始日期和结束日期
+    let startDate = new Date('2018-01-01');
+    let endDate = new Date();
+
+    // 格式化日期为 'YYYY-MM-DD'
+    function formatDate(date) {
+        let year = date.getFullYear();
+        let month = (date.getMonth() + 1).toString().padStart(2, '0');
+        let day = date.getDate().toString().padStart(2, '0');
+        return `${year}-${month}-${day}`;
+    }
+
+    // 循环输出从起始日期到结束日期的每一天
+    while (startDate <= endDate) {
+        await doUpdateScheduleGroup(formatDate(startDate));
+        await doUpdateTeam(formatDate(startDate));
+        await doUpdateGame(formatDate(startDate));
+
+        // 增加一天
+        startDate.setDate(startDate.getDate() + 1);
+    }
 }
 
-async function doUpdateScheduleGroup() {
+async function doUpdateScheduleGroup(startDate) {
     for (let i = 0; i < 1; i++) {
         await sleep(1000);
         let reqUrl = ''
@@ -114,8 +152,8 @@ async function doUpdateScheduleGroup() {
 
         const urlObj = new URL('https://extra.365-023.com/ResultsApi/GetCompetitions?sportId=1&fromDate=2024-05-18&toDate=2024-05-19&lng=10');
         const searchParamsObj = new URLSearchParams(urlObj.search);
-        searchParamsObj.set('fromDate', '2024-05-14')
-        searchParamsObj.set('toDate', '2024-05-14')
+        searchParamsObj.set('fromDate', startDate)
+        searchParamsObj.set('toDate', startDate)
 
         searchParamsObj.set('lng', languageIDEn)
         reqUrl = urlObj.origin + urlObj.pathname + '?' + searchParamsObj.toString()
@@ -165,7 +203,7 @@ async function doUpdateScheduleGroup() {
     }
 }
 
-async function doUpdateTeam() {
+async function doUpdateTeam(startDate) {
     for (let i = 0; i < 1; i++) {
         await sleep(1000);
         let reqUrl = ''
@@ -177,8 +215,8 @@ async function doUpdateTeam() {
 
         const urlObj = new URL('https://extra.365-023.com/ResultsApi/GetTeams?sportId=1&fromDate=2024-05-14&toDate=2024-05-14&ct=42&lng=1&st=108&tz=GMTPlus8&ot=Decimals');
         const searchParamsObj = new URLSearchParams(urlObj.search);
-        searchParamsObj.set('fromDate', '2024-05-14')
-        searchParamsObj.set('toDate', '2024-05-14')
+        searchParamsObj.set('fromDate', startDate)
+        searchParamsObj.set('toDate', startDate)
 
         searchParamsObj.set('lng', languageIDEn)
         reqUrl = urlObj.origin + urlObj.pathname + '?' + searchParamsObj.toString()
@@ -222,6 +260,119 @@ async function doUpdateTeam() {
                                  WHERE language_id = ${teams[j].language_id}
                                    AND team_id = ${teams[j].team_id}
                                    AND nickname = '${teams[j].nickname}');
+            `
+            await apiSqlExec(sqlExec);
+        }
+    }
+}
+
+async function doUpdateGame(startDate) {
+    for (let i = 0; i < 1; i++) {
+        await sleep(1000);
+        let reqUrl = ''
+        let respEn = ''
+        let respCh = ''
+        let languageIDEn = '1'
+        let languageIDCh = '10'
+        let sqlExec
+
+        const urlObj = new URL('https://extra.365-023.com/ResultsApi/GetFixtures?sportId=1&competitionId=999999&challengeId=0&fixtureId=0&fromDate=2024-05-15&toDate=2024-05-15&isDynamic=false&linkId=0&teamId=0&sportDescriptor=%E8%B6%B3%E7%90%83&isVirtual=false&ct=42&lng=10&st=108&tz=GMTPlus8&ot=Decimals');
+        const searchParamsObj = new URLSearchParams(urlObj.search);
+        searchParamsObj.set('fromDate', startDate)
+        searchParamsObj.set('toDate', startDate)
+
+        searchParamsObj.set('lng', languageIDCh)
+        reqUrl = urlObj.origin + urlObj.pathname + '?' + searchParamsObj.toString()
+        respCh = await curlGet(reqUrl)
+        if (!respCh || !respCh.fixtures || respCh.fixtures.length === 0) {
+            console.error('!respCh || !respCh.fixtures || respCh.fixtures.length === 0', reqUrl)
+            continue
+        }
+
+        let games = []
+        $.each(respCh.fixtures, function (key, value) {
+            if (!value.fixtureId) {
+                console.error('!value.fixtureId', reqUrl, value.fixtureId)
+                return
+            }
+
+            if (!value.challengeId) {
+                console.error('!value.challengeId', reqUrl, value.challengeId)
+                return
+            }
+
+            let descArr = value.desc.split(' v ')
+            if (!descArr || descArr.length !== 2) {
+                console.error('descArr.length !== 2', reqUrl, value.desc)
+                return
+            }
+
+            if (!value.userTime) {
+                console.error('!value.userTime', reqUrl, value.userTime)
+                return
+            }
+            let gametimestamp = new Date(value.userTime).getTime() / 1000
+            let gametime = timestamp2Gametime(gametimestamp)
+
+            games.push({
+                id: value.fixtureId,
+                gametimestamp: gametimestamp,
+                gametime: gametime,
+                classid: value.challengeId,
+                homename: descArr[0],
+                guestname: descArr[1],
+            })
+        });
+
+        for (let j = 0; j < games.length; j++) {
+            let gamename = ''
+            let sqlRaw = `SELECT *
+                          FROM bet365_schedule_group_nickname
+                          WHERE language_id = ${languageIDCh}
+                            AND schedule_group_id = ${games[j].classid} LIMIT 1`
+            let resp = await apiSqlRaw(sqlRaw)
+            if (resp && resp.length > 0) {
+                gamename = resp[0].nickname
+            }
+
+            let homeid = 0
+            sqlRaw = `SELECT *
+                      FROM bet365_team_nickname
+                      WHERE language_id = ${languageIDCh}
+                        AND nickname = '${games[j].homename}' LIMIT 1`
+            resp = await apiSqlRaw(sqlRaw)
+            if (resp && resp.length > 0) {
+                homeid = resp[0].team_id
+            }
+
+            let guestid = 0
+            sqlRaw = `SELECT *
+                      FROM bet365_team_nickname
+                      WHERE language_id = ${languageIDCh}
+                        AND nickname = '${games[j].homename}' LIMIT 1`
+            resp = await apiSqlRaw(sqlRaw)
+            if (resp && resp.length > 0) {
+                guestid = resp[0].team_id
+            }
+
+            sqlExec = `
+                INSERT INTO bet365_info (id, gametimestamp, gametime, classid, gamename, homeid, homename, guestid,
+                                         guestname, url, api_resp)
+                SELECT ${games[j].id},
+                       ${games[j].gametimestamp},
+                       '${games[j].gametime}',
+                       ${games[j].classid},
+                       '${gamename}',
+                       ${homeid},
+                       '${games[j].homename}',
+                       ${guestid},
+                       '${games[j].guestname}',
+                       '',
+                       ''
+                FROM DUAL
+                WHERE NOT EXISTS(SELECT *
+                                 FROM bet365_info
+                                 WHERE id = ${games[j].id});
             `
             await apiSqlExec(sqlExec);
         }
@@ -286,6 +437,8 @@ async function main() {
         await doUpdateScheduleGroup();
     } else if (location.pathname === '/ResultsApi/GetTeams') {
         await doUpdateTeam();
+    } else if (location.pathname === '/ResultsApi/GetFixtures') {
+        await doUpdateGame();
     } else if (location.pathname === '/ResultsApi/GetResults') {
         await doUpdateScore();
     }
