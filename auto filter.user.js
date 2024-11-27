e13ec70ab336a19fc08148e6a68c841a6815a2ba
lennon <lennonandjune@gmail.com>
2024-11-27 14:11:04 +0800

    feat: auto filter.user.js oms2.yunexpress.cn

--- auto filter.user.js
+++ auto filter.user.js
@@ -4,6 +4,7 @@
 // @description  filter something you don't need or make some operation easy
 // @author       Lennon
 // @match        https://www.tapd.cn/*
+// @match        https://oms2.yunexpress.cn/*
 // @require      https://cdn.staticfile.org/jquery/3.4.0/jquery.min.js
 // @run-at       document-end
 // @icon         http://icons.iconarchive.com/icons/iconshock/cms/128/user-login-icon.png
@@ -13,6 +14,8 @@
 setTimeout(function () {
     if ($.inArray(location.hostname, ['www.tapd.cn']) >= 0) {
         siteTapd();
+    } else if ($.inArray(location.hostname, ['oms2.yunexpress.cn']) >= 0) {
+        siteYunExpress();
     }
 }, 3000);
 
@@ -32,4 +35,8 @@ function siteTapd() {
     clipboardTextNew = clipboardTextNew.join(' ')
     clipboardTextNew = clipboardTextNew.replace('  ', ' ')
     element.attr('data-clipboard-text', clipboardTextNew)
+}
+
+function siteYunExpress() {
+    document.getElementsByClassName('user-name').innerHTML = '欧阳'
 }
